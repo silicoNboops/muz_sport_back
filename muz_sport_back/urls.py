28b69db7df4230f11b7189ad260870791c3ee47e
Maxat Kasymov <maxlestor2@gmail.com>
2022-05-18 04:27:03 +0600

    New views Orders, new urls orders

--- muz_sport_back/urls.py
+++ muz_sport_back/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
+from muzsport.views import OrderViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'orders', OrderViewSet, basename='order')
+urlpatterns = router.urls
+
+urlpatterns += [
     path('admin/', admin.site.urls),
-    # path('auth/', include("djoser.urls")),
-    # path('auth/', include("djoser.urls.authtoken")),
     path('auth/', include("djoser.urls.jwt")),
+    path('auth/', include("djoser.urls")),
+    path('auth/', include("djoser.urls.authtoken")),
 ]
--- muzsport/views.py
+++ muzsport/views.py
@@ -1,1 +1,11 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
+from muzsport.models import Order
+from muzsport.serializers import OrderSerializers
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = OrderSerializers
+    queryset = Order.objects.all()
