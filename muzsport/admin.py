69f858fcbc37e690bef20fb421e19e6a08332924
Maxat Kasymov <maxlestor2@gmail.com>
2022-05-24 22:37:27 +0600

    Fix models

--- muzsport/admin.py
+++ muzsport/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from muzsport.models import Track, Coupons, Order
+from muzsport.models import *
 from django.contrib.auth.models import Group
 
 admin.site.register(Track)
 admin.site.register(Coupons)
 admin.site.register(Order)
 admin.site.unregister(Group)
+admin.site.register(Sports)
+admin.site.register(Tags)
+admin.site.register(Moods)
+admin.site.register(Country)
 
--- muzsport/models.py
+++ muzsport/models.py
@@ -1,65 +1,81 @@
 from django.db import models
 
 
-STATUS_CHOICES =(
-    ("Active", "Активен"),
-    ("Disable", "Не активен"),
-)
+class Sports(models.Model):
+    sports_name = models.CharField(max_length=30, null=True, blank=True, verbose_name='Наименование спорта')
+
+    def __str__(self):
+        return str(self.sports_name)
+
+    class Meta:
+        verbose_name = 'Вид спорта'
+        verbose_name_plural = 'Виды спорта'
 
 
-END_CHOICES =(
-    ("Smooth", "Плавное"),
-    ("Sharp", "Резкое"),
-)
+class Tags(models.Model):
+    tag_name = models.CharField(max_length=30, null=True, blank=True, verbose_name='Хэштег')
+
+    def __str__(self):
+        return str(self.tag_name)
+
+    class Meta:
+        verbose_name = 'Хэштеги'
+        verbose_name_plural = 'Хэштег'
 
 
-DIRECTION_EFFECT =(
-    ("Concentration", "На концетрацию"),
-    ("Stamina", "На выносливость"),
-    ("Force", "На силу и преодоление"),
-)
+class Moods(models.Model):
+    mood_name = models.CharField(max_length=30, null=True, blank=True, verbose_name='Настроение')
+
+    def __str__(self):
+        return str(self.mood_name)
+
+    class Meta:
+        verbose_name = 'Настроения'
+        verbose_name_plural = 'Настроение'
 
 
-ORDER_STATUS =(
-    ("Performed", "Выполнен"),
-    ("Process", "В обработке"),
-)
+class Country(models.Model):
+    country_name = models.CharField(max_length=30, null=True, blank=True, verbose_name='Страна')
+
+    def __str__(self):
+        return str(self.country_name)
+
+    class Meta:
+        verbose_name = 'Страны'
+        verbose_name_plural = 'Страна'
 
 
 class Track(models.Model):
+    file = models.FileField(null=True, blank=True, upload_to="mp4", verbose_name='Трэк')
+    photo = models.CharField(max_length=150, null=True, blank=True, verbose_name='Фото')
     author = models.CharField(max_length=150, null=True, blank=True, verbose_name='Автор')
     title = models.CharField(max_length=150, null=True, blank=True, verbose_name='Название трека')
-    sport_name = models.CharField(max_length=150, null=True, blank=True, verbose_name='Вид спорта')
     price = models.IntegerField(null=True, blank=True, verbose_name='Цена')
-    beginning_peak = models.BooleanField(default=True, verbose_name='Пик в начале')
-    end = models.CharField(max_length=32, choices=END_CHOICES, verbose_name='Окончание')
-    direction_of_effect = models.CharField(max_length=64, choices=DIRECTION_EFFECT,verbose_name='Направление воздействия'
-                                                                                                ' эффекта')
-    # TODO подумать реализацию добавление трека
-    # add_track = models.
-    sportsman_name = models.CharField(max_length=100, null=True, blank=True, verbose_name='Имя спортсмена')
-    photo = models.CharField(max_length=150, null=True, blank=True, verbose_name='Фото')
     track_length = models.IntegerField(null=True, blank=True, verbose_name='Длительность трека')
+    sports_name = models.ForeignKey(Sports, on_delete=models.CASCADE, verbose_name='Наименование спорта')
+    tag_name = models.ManyToManyField('Tags', verbose_name='Хэштег')
+    mood_name = models.ManyToManyField('Moods', verbose_name='Настроение')
+    country_name = models.ForeignKey(Country, on_delete=models.CASCADE, verbose_name='Страна')
+    with_words = models.BooleanField(default=False, verbose_name='Со словами?')
 
     def __str__(self):
-        return str(self.title)
+        return f"{self.title}-{self.author}"
 
     class Meta:
-        ordering = ['id']
         verbose_name = 'Трек'
         verbose_name_plural = 'Треки'
 
 
 class Coupons(models.Model):
-    # TODO Привязка к id user'a
-    validity_period = models.CharField(max_length=20, null=True, blank=True, verbose_name='Срок действия')
-    status = models.CharField(max_length=32, choices=STATUS_CHOICES, default='Active')
+    coupon_name = models.CharField(max_length=20, null=True, blank=True, verbose_name='Купон', unique=True)
+    validity_period = models.DateField(null=True, blank=True, verbose_name='Срок действия')
+    status = models.BooleanField(default=True, verbose_name='Статус')
+    percent = models.IntegerField(null=True, blank=True, verbose_name='Процент')
 
     def __str__(self):
-        return str(self.id)
+        return f"{self.coupon_name}"
 
     class Meta:
-        ordering = ['id']
         verbose_name = 'Купон'
         verbose_name_plural = 'Купоны'
 
@@ -69,15 +85,13 @@ class Order(models.Model):
     last_name = models.CharField(max_length=20, null=True, blank=True, verbose_name='Фамилия заказчика')
     phone = models.CharField(max_length=30, null=True, blank=True, verbose_name='Номер телефона')
     email = models.CharField(max_length=256, null=True, blank=True, verbose_name='E-Mail')
-    order_number = models.AutoField(primary_key=True, verbose_name='Номер заказа')
     order_date = models.DateField(auto_now_add=True, verbose_name='Дата заказа')
-    status = models.CharField(max_length=64, choices=ORDER_STATUS, default='Process', verbose_name='Статус заказа')
     price = models.IntegerField(null=True, blank=True, verbose_name='Стоимость')
 
     def __str__(self):
         return str(self.order_number)
 
     class Meta:
-        ordering = ['order_number']
         verbose_name = 'Заказ'
         verbose_name_plural = 'Заказы'
+
