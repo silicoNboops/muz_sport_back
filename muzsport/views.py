6dfcb91dd33154a3417397b22569256f51e3dfd8
jsliq_d <egyabig2@gmail.com>
2022-06-03 21:17:02 +0300

    create coupon api

--- muzsport/views.py
+++ muzsport/views.py
@@ -1,4 +1,7 @@
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets
+from rest_framework.parsers import JSONParser
+
 from muzsport.models import *
 from muzsport.serializers import *
 from django_filters.rest_framework import DjangoFilterBackend
@@ -35,4 +38,18 @@ class TrackReadOnlyModelViewSet(viewsets.ReadOnlyModelViewSet):
 
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['sports_name', 'tag_name', 'mood_name', 'country_name', 'with_words']
+
+
+@csrf_exempt
+def coupon_api(request):
+    if request.method == 'GET':
+        coupon_name = request.get('coupon_name')
+        c = Coupons.objects.filter(coupon_name=coupon_name).first()
+
+        print(c)
+
+        if c:
+            return c
+        else:
+            return None
 
