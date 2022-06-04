e11df43cd7f271fbae6a0f4f578f864a025837d6
jsliq_d <egyabig2@gmail.com>
2022-06-04 12:00:55 +0300

    fix dev allowed_hosts 2

--- muz_sport_back/settings.py
+++ muz_sport_back/settings.py
@@ -63,7 +63,7 @@ CORS_ORIGIN_ALLOW_ALL = False
 
 # СЮДА ТОЖ НАДО ВПИСЫВАТЬ ИХ
 CORS_ORIGIN_WHITELIST = ('http://localhost:3000', 'http://localhost:8000',
-                         'http://ovz1.kupalex.n03kn.vps.myjino.ru')
+                         'http://ovz1.kupalex.n03kn.vps.myjino.ru', '81.177.165.19')
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOW_METHODS = [
