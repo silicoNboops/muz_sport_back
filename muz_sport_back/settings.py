565e6d1f00acd4536025b34f0f0cf8a7b59f227d
squalorDf <egyabig2@gmail.com>
2022-07-12 14:13:41 +0300

    fix csrf checking sessionAuthentication

--- muz_sport_back/settings.py
+++ muz_sport_back/settings.py
@@ -104,7 +104,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
